8d0bf78740283a12ef8673551e65441c0597206b
Pradeep Sanap <21pss16028@gmail.com>
2026-07-04 05:47:04 +0000

    memory mangement updated

---
+++ .devcontainer/01_basic/datatypeRevesion.js
@@ -0,0 +1,61 @@
+/*
+dynamic lanuage are slow as compared to static for execuataion but fast for protyping ok 
+
+
+JavaScript is a dynamically typed language. This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime.
+
+For example, in JavaScript, you can do the following:
+
+let x = 10; // x is now a number
+x = "Hello"; // x is now a string
+x = true; // x is now a boolean
+On the other hand, statically typed languages require you to declare the variable's type explicitly, and the type checking is done at compile-time, before the code is executed.
+
+Languages like Java, C++, and TypeScript are statically typed, and they require you to specify the variable type explicitly when declaring them:
+
+int x = 10; // x is a variable of type int
+String name = "John"; // name is a variable of type String
+JavaScript's dynamic typing allows for more flexibility but can lead to potential runtime errors if not handled carefully. Static typing, on the other hand, provides better type safety at the cost of some initial verbosity and strictness.
+
+
+
+
+Return type of variables in JavaScript
+
+
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object const heros=["",""]
+       Function  =>  function object ( means object only )
+      const name = function myname(){} 
+       Object  =>  object  my object ={} key value pair 
+
+
+
+
+
+
+
+
+
+    imp these is imp ;
+
+    stack(used for permvitive ) and heap (used for non permivitive)
+
+stack it return the copy of of variable means not the orginal value is given to you , so old value is persevee
+
+//heap the name of variable is store in stack and it point to the heap memory which stores the values and it return the orginal value not the copy 
+
+
+
+
+
+*/
