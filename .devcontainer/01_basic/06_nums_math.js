707af9126ae8bd1fcce84f9589970cb0e4f5960a
Pradeep Sanap <21pss16028@gmail.com>
2026-07-05 07:06:05 +0000

    baisc done

---
+++ .devcontainer/01_basic/06_nums_math.js
@@ -0,0 +1,40 @@
+//way of creating the number variable
+const score=400;
+
+const out =new Number(100);
+
+//toString() method convert the number to string 
+
+console.log(out.toFixed(2));
+//these is the method in which fixed decimal tell that will come ;
+
+//toprecsion method  signicant digit and it return the string so we can apply string method also 
+
+let num=100000;
+
+
+console.log(num.toLocaleString('en-IN'));
+
+//max_value,min_value 
+
+//++++++++++++++++++++Math++++++++++++++
+
+//.abs means make negative number to positive
+//.round it roundoff the values
+// .ceil roundoff to the higher value 
+// .floor round off to lower value 
+// .min return min value 
+// .max return max value 
+//.sqrt sqaure root 
+// .pow
+// .random return random value between value bewteen 0 to 1
+ /*
+
+ math.floor(math.random()*(max-min+1)+min)
+ */
+
+ let min=300;
+ let max=500;
+let randomNumber =Math.floor(Math.random()*(max-min +1)+min);
+
+console.log(randomNumber);
---
+++ .devcontainer/01_basic/07_date.js
@@ -0,0 +1,21 @@
+//date are very imp in real world project 
+// let mydate= new Date();
+// //it is can object 
+// console.log(mydate);//have particular format
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+
+// let mycreatedate=new Date(2023,0,23);
+// console.log(mycreatedate);
+// //in these declaration month start with 0;
+// let newdate = new Date("2005-5-18");
+// console.log(newdate)
+
+
+let timestamp=Math.floor((Date.now()/1000))
+
+
+console.log(new Date(timestamp * 1000));
+
