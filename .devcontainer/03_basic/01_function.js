4976228666960228ca01fec5ecebf01c10114648
Pradeep Sanap <21pss16028@gmail.com>
2026-07-07 06:28:21 +0000

    done function scope left

---
+++ .devcontainer/03_basic/01_function.js
@@ -0,0 +1,38 @@
+//function is that container which increse the reusablity of code and that packes the code  in on one packet and acess 
+
+
+//synax of function making and calling 
+// function sayMyName(){
+//     console.log("My name is Pradeep ");
+// }
+
+// sayMyName();
+//only satMyName just show the address that function exsit here to call we use () then it get execute 
+
+
+
+function addTwo(num1 ,num2){ // value passed are parameter
+    return num1+num2;
+}
+
+let sum=addTwo(4,5);//value passed are argunment 
+console.log(sum);
+
+//if I don't passed any value it shows undefined 
+
+
+//can give the default value also by (name = "pradeep") these will be treated as deault 
+
+
+//(!undefined ) undefined is consider as flase when it comes it will covert to true then if condition run 
+
+
+
+//the number of parametr pass are more we take only one so these can be handle with (...rest operater) it store all parameter in array then we can acess and get one one value 
+//function ( ...myname)
+
+
+//object and array in function pass directly or pass with variable 
+
+//imp check the type of data type ,isaaviable or not 
+
