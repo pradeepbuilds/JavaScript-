ef247f00bf1928efee7684e5ff302eca415644a7
Pradeep Sanap <21pss16028@gmail.com>
2026-07-12 14:13:58 +0530

    Enhance function examples in 01_function.js
    Added examples of function usage with objects and arrays, including printPerson and printNumbers functions.

--- .devcontainer/03_basic/01_function.js
+++ .devcontainer/03_basic/01_function.js
@@ -6,6 +6,8 @@
 //     console.log("My name is Pradeep ");
 // }
 
+
+
 // sayMyName();
 //only satMyName just show the address that function exsit here to call we use () then it get execute 
 
@@ -30,9 +32,59 @@ console.log(sum);
 
 //the number of parametr pass are more we take only one so these can be handle with (...rest operater) it store all parameter in array then we can acess and get one one value 
 //function ( ...myname)
+// //function sayname(a,...b){
+// return a;
+// }
+//sayname(2,3,4,5,5)//the value 2 will store in a and other will store in array of b it rest operator 
+
+//methodcurry 
+/*
+sayname(2,3);
+sayname(2)(3);
+
+function syname(a,b){
+
+ if(b !=undefined){return a+b
+
+ else return function sayname(b){
+ return a+b
+
+}
+
+
+*/
+
 
 
 //object and array in function pass directly or pass with variable 
+function printPerson(person) {
+  console.log("Name:", person.name);
+  console.log("Age:", person.age);
+}
+
+// Example object
+const personObj = { name: "Pradeep", age: 22 };
+
+printPerson(personObj);
+// Output:
+// Name: Pradeep
+// Age: 22
+function printNumbers(numbers) {
+  numbers.forEach(num => console.log(num));
+}
+
+// Example array
+const numArray = [10, 20, 30, 40];
+
+printNumbers(numArray);
+// Output:
+// 10
+// 20
+// 30
+// 40
+
+
+
 
 //imp check the type of data type ,isaaviable or not 
 
